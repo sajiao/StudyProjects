c595ce2fefb8facf9f6b75aa5778bb789fca8372
eleven <yijun@moqihuyu.com>
2018-12-14 08:49:34 +0800

    delete file

---
+++ Web/WebAPI/Controllers/UpLoadController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [Consumes("application/json", "multipart/form-data")]//此处为新增
+    [ApiController]
+    public class UpLoadController : BaseController
+    {
+        [HttpPost]
+        [HttpPost("upload")]
+        public ResponseResult upload(IFormFile file, string userId)
+        {
+            if (file != null)
+            {
+                var fileDir = AppContext.BaseDirectory;
+                if (!Directory.Exists(fileDir))
+                {
+                    Directory.CreateDirectory(fileDir);
+                }
+                //文件名称
+                string projectFileName = file.FileName;
+
+                //上传的文件的路径
+                string filePath = fileDir + $@"\{projectFileName}";
+                using (FileStream fs = System.IO.File.Create(filePath))
+                {
+                    file.CopyTo(fs);
+                    fs.Flush();
+                }
+                return new ResponseResult(0, "ok");
+            }
+            else
+            {
+                return new ResponseResult(0, "no");
+            }
+        }
+
+    }
+}
---
+++ Web/WebAPI/Controllers/UserFileController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserFileController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromFile]UserFile file)
+        {
+            if (file == null || !file.IsValid)
+                return new JsonResult(new { code = 500, message = "不允许上传的文件类型" });
+            string newFile = string.Empty;
+            if (file != null)
+                newFile = await file.SaveAs("/data/files/images");
+            return new JsonResult(new { code = 0, message = "成功", url = newFile });
+        }
+    }
+}
