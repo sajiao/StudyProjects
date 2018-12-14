2796275fa66b4a15f0207b96adde0a8725e21ed4
eleven <yijun@moqihuyu.com>
2018-12-14 15:49:11 +0800

    add read pdf

---
+++ Web/DotNet.Common/Helper/PDFHelper.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Spire.Pdf;
+
+namespace DotNet.Common.Helper
+{
+    public class PDFHelper
+    {
+        public static string ReadPdfContent(string filepath)
+        {
+            try
+            {
+                //实例化一个PdfDocument对象
+                PdfDocument doc = new PdfDocument();
+
+                //加载PDF文档
+                doc.LoadFromFile(filepath);
+
+                //实例化一个StringBuilder 对象
+                StringBuilder content = new StringBuilder(500);
+
+                //提取PDF所有页面的文本
+                foreach (PdfPageBase page in doc.Pages)
+                {
+                    content.Append(page.ExtractText());
+                }
+
+                var text = content.ToString();
+                return text;
+            }
+            catch (Exception ex)
+            {
+               
+                Logger.WriteErrorLog("出错文件：" + filepath + "原因：" + ex.ToString());
+                return null;
+            }
+        }
+
+        public static List<string> ReadPDFLine(string filepath)
+        {
+            string content = ReadPdfContent(filepath);
+            var arr = content.Split('\n');
+            List<string> result = new List<string>(arr.Length);
+            foreach (var item in arr)
+            {
+                if (item.IsNullOrEmpty()) continue;
+
+                var temp = item.Trim('\t').Trim();
+
+                if (temp.IsNullOrEmpty()) continue;
+
+                result.Add(temp.Trim('\r').Trim('\n'));
+            }
+
+            return result;
+        }
+    }
+}
--- Web/WebAPI/Controllers/UpLoadController.cs
+++ Web/WebAPI/Controllers/UpLoadController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNet.Common;
+using DotNet.Common.Helper;
 using Entities;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,6 +17,47 @@ namespace WebAPI.Controllers
     [ApiController]
     public class UpLoadController : BaseController
     {
+
+        [HttpGet]
+        public ResponseResult Get()
+        {
+           
+            var fileDir = Path.Combine(AppContext.BaseDirectory,"Upload");
+            if (!Directory.Exists(fileDir))
+            {
+                Directory.CreateDirectory(fileDir);
+            }
+            //文件名称
+            string projectFileName = "词霸天下38000词汇大全集 第1部分.pdf";
+
+            //上传的文件的路径
+            string filePath = fileDir + $@"\{projectFileName}";
+            List<string> content = null;
+            if (FileHelper.IsExistFile(filePath))
+            {
+               content =   PDFHelper.ReadPDFLine(filePath);
+            }
+
+            FilterEtyma(content);
+            return new ResponseResult(0,"", content);
+           
+        }
+
+        private List<string> FilterEtyma(List<string> contents)
+        {
+            List<string> filters = new List<string> { "Evaluation Warning : The document was created with Spire.PDF for .NET", "宋老师终极词汇完美教程", "宋老师超越母语者完美版", "终极词汇速记", "组词根综合", "课后问答联系方式", "宋老师口语素材" , "高考高分高能句型全总结", "宋老师英语词汇速记教材", "神奇后缀篇", "宋老师独家高中", "神奇前缀", "宋老师母语级词汇完美教程", "宋老师词霸天下系列", "母语级词汇速记", "词根统计100", "累积解析词汇量" };
+
+            for (var i = contents.Count - 1; i >= 0; i--)
+            {
+                if (filters.Any(f => contents[i].Contains(f)))
+                {
+                    contents.RemoveAt(i);
+                }
+            }
+
+            return contents;
+        }
+
         [HttpPost]
         [HttpPost("upload")]
         public ResponseResult upload(IFormFile file, string userId)
